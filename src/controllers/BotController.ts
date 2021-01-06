e9a7f08571de863755abe3ee1171b7617c03db4d
pders01 <paulderscheid@gmail.com>
2021-01-06 18:12:55 -0500

    Initial commit

---
+++ src/controllers/BotController.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from "express";
+import { Controller, Post } from "@overnightjs/core";
+import { Logger } from "@overnightjs/logger";
+import { runQuery } from "../utils/dialogflow";
+
+@Controller("api/bot")
+export class BotController {
+  @Post()
+  private postMessage(request: Request, response: Response) {
+    // Here we get the message body, the id to which we're sending the message and where it comes from.
+    const { Body, To, From } = request.body;
+    // Here we're sending the received message to Dialogflow (dialogflow.ts) so that it can be identified against an Intent.
+    runQuery(Body)
+      .then((result: any) => {
+      // Now the fulfillment text has to make its way back to the frontend
+      const message = {
+        From: From, 
+          message: {
+          To: To, 
+          Body: result.fulfillmentText
+          }
+      }  
+      console.log(message)
+      return response.status(200).send(message); 
+      })
+      .catch((error) => {
+        console.error("error is ", error);
+        Logger.Err(error);
+      });
+      
+    // return response.status(200).send("SUCCESS");
+  }
+}
---
+++ src/utils/dialogflow.ts
@@ -0,0 +1,45 @@
+// dialogflow.ts
+import { v4 as uuidv4 } from 'uuid';
+
+
+const dialogflow = require("dialogflow");
+const credentials = require("../../credentials.json");
+
+const sessionClient = new dialogflow.SessionsClient({
+  credentials: credentials
+});
+const projectId: string = process.env.DIALOGFLOW_PROJECT_ID!;
+
+export const runQuery = (query: string) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      // A unique identifier for the given session
+      const sessionId = uuidv4();
+      //const sessionId = number;
+      // Create a new session
+
+      const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+
+      // The text query request.
+      const request = {
+        session: sessionPath,
+        queryInput: {
+          text: {
+            // The query to send to the dialogflow agent
+            text: query,
+            // The language used by the client (en-US)
+            languageCode: "de-DE"
+          }
+        }
+      };
+
+      // Send request and log result
+      const responses = await sessionClient.detectIntent(request);
+
+      const result = responses[0].queryResult;
+      resolve(result);
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
